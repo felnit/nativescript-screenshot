162193b1070d986e20ab8225f9fd9cdca7ac4e05
f.nithammer@mammut-group.de <felixnithammer@Felix-MBP.fritz.box>
2019-03-13 09:36:55 +0100

    function for ios11

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 var imageSource = require("image-source");
 
 exports.getImage = function(view) {
-    if (view.ios) {
-        UIGraphicsBeginImageContextWithOptions(view.ios.frame.size, false, 0);
-        view.ios.drawViewHierarchyInRectAfterScreenUpdates(CGRectMake(0, 0, view.ios.frame.size.width, view.ios.frame.size.height), true);
-        var imageFromCurrentImageContext = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        return imageSource.fromData(UIImagePNGRepresentation(imageFromCurrentImageContext));
-        
-    } else if (view.android) {
-        view.android.setDrawingCacheEnabled(true);
-        var bmp = android.graphics.Bitmap.createBitmap(view.android.getDrawingCache());
-        view.android.setDrawingCacheEnabled(false);
-        var source = new imageSource.ImageSource();
-        source.setNativeSource(bmp);
-        return source;
-    }
-    return undefined;
-}
+	if (view.ios) {
+		let layer = args.ios.keyWindow.layer;
+		let scale = UIScreen.main.scale;
+		UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, scale);
+		layer.renderInContext(UIGraphicsGetCurrentContext());
+		screenshotImage = UIGraphicsGetImageFromCurrentImageContext();
+		UIGraphicsEndImageContext();
+		return screenshotImage;
+	} else if (view.android) {
+		view.android.setDrawingCacheEnabled(true);
+		var bmp = android.graphics.Bitmap.createBitmap(view.android.getDrawingCache());
+		view.android.setDrawingCacheEnabled(false);
+		var source = new imageSource.ImageSource();
+		source.setNativeSource(bmp);
+		return source;
+	}
+	return undefined;
+};
